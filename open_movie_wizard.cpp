3442b9ca63ebdebd75b3d43488040765a03d9696
Lukas Waymann <lb.waym@gmail.com>
2015-02-18 13:10:18 +0100

    Update formatting, add modelines

--- open_movie_wizard.cpp
+++ open_movie_wizard.cpp
@@ -14,243 +14,257 @@
 
 class OpenMovieWizard::DirDialogPage : public wxWizardPageSimple
 {
-	public:
+   public:
 
-	DirDialogPage(OpenMovieWizard* parent, wxString* dir = nullptr, const wxFileHistory* dirHistory = nullptr);
+   DirDialogPage(OpenMovieWizard* parent, wxString* dir = nullptr,
+      const wxFileHistory* dirHistory = nullptr);
 
-	void onButton(wxCommandEvent&); // process a wxEVT_COMMAND_BUTTON_CLICKED
+   void onButton(wxCommandEvent&); // process a wxEVT_COMMAND_BUTTON_CLICKED
 
-	private:
+   private:
 
-	struct DirValidator : public wxValidator
-	{
-		DirValidator(wxString* value) : wxValidator{}, value{value} {}
+   struct DirValidator : public wxValidator
+   {
+      DirValidator(wxString* value) : wxValidator{}, value{value} {}
 
-		DirValidator(const DirValidator& foo) : wxValidator{}, value{foo.value} {}
+      DirValidator(const DirValidator& foo) : wxValidator{}, value{foo.value} {}
 
-		virtual bool Validate(wxWindow*);  // override
-		virtual bool TransferFromWindow(); // override
-		virtual bool TransferToWindow();   // override
+      virtual bool Validate(wxWindow*);  // override
+      virtual bool TransferFromWindow(); // override
+      virtual bool TransferToWindow();   // override
 
-		virtual wxObject* Clone() const; // override
+      virtual wxObject* Clone() const; // override
 
-		private:
+      private:
 
-		wxString* value;
-	};
+      wxString* value;
+   };
 
-	wxComboBox* comboBox;
+   wxComboBox* comboBox;
 };
 
 class OpenMovieWizard::RegExCtrlPage : public wxWizardPageSimple
 {
-	public:
+   public:
 
-	RegExCtrlPage(OpenMovieWizard* parent, wxString* regEx, wxConfigBase* = nullptr);
+   RegExCtrlPage(OpenMovieWizard* parent, wxString* regEx, wxConfigBase* = nullptr);
 
-	private:
+   private:
 
-	wxComboBox* comboBox;
+   wxComboBox* comboBox;
 };
 
-OpenMovieWizard::OpenMovieWizard(wxWindow* parent, int id, wxString* dir, wxString* regEx, wxConfigBase* config,
-	const wxFileHistory* dirHist) :
-	wxWizard{parent, id, u8"Open Movie Wizard"},
-	dirPage{new DirDialogPage{this, dir, dirHist}},
-	regExPage{new RegExCtrlPage{this, regEx, config}}
+OpenMovieWizard::OpenMovieWizard(wxWindow* parent, int id, wxString* dir, wxString* regEx,
+   wxConfigBase* config, const wxFileHistory* dirHist) :
+   wxWizard{parent, id, u8"Open Movie Wizard"},
+   dirPage{new DirDialogPage{this, dir, dirHist}},
+   regExPage{new RegExCtrlPage{this, regEx, config}}
 {
-	wxWizardPageSimple::Chain(dirPage, regExPage);
+   wxWizardPageSimple::Chain(dirPage, regExPage);
 
-	// here, "this will enlarge the wizard to fit the biggest page"; see the documentation of
-	GetPageAreaSizer()->Add(dirPage); // wxWizard::GetPageAreaSizer() in the wxWizard Class Reference
+   // Here, "this will enlarge the wizard to fit the biggest page"; see the documentation
+   GetPageAreaSizer()->Add(dirPage); // of wxWizard::GetPageAreaSizer().
 }
 
 bool OpenMovieWizard::RunWizard()
 {
-	return wxWizard::RunWizard(dirPage);
+   return wxWizard::RunWizard(dirPage);
 }
 
-OpenMovieWizard::DirDialogPage::DirDialogPage(OpenMovieWizard* parent, wxString* dir, const wxFileHistory* dirHist) :
-	wxWizardPageSimple{parent}
+OpenMovieWizard::DirDialogPage::DirDialogPage(OpenMovieWizard* parent, wxString* dir,
+   const wxFileHistory* dirHist) :
+   wxWizardPageSimple{parent}
 {
-	//// <_configuration_information_handling_> ////
-	///
-	// note that the wizard itself does not store any directories; it could if it would save the
-	// wxConfig object (that might be reasonable) - this page doesn't store them either (tho, I guess,
-	// it could do so from its destructor)
+   //// <_configuration_information_handling_> ////
+   ///
+   // Note that the wizard itself does not store any directories; it could if it would
+   // save the wxConfig object (that might be reasonable) - this page doesn't store them
+   // either (tho, I guess, it could do so from its destructor).
 
-	// has, as per its documentation, the "full set of std::vector<wxString> compatible methods";
-	wxArrayString dirs{}; // I'll therefore use it as if it were a vector
+   // Has, as per its documentation, the "full set of std::vector<wxString> compatible
+   // methods"; I'll therefore use it as if it were a vector.
+   wxArrayString dirs{};
 
-	if (dirHist) // was a wxFileHistory provided?
-	{
-		for (std::size_t i = 0; i < dirHist->GetCount(); ++i)
-			dirs.push_back(dirHist->GetHistoryFile(i));
-	}
+   if (dirHist) // was a wxFileHistory provided?
+   {
+      for (std::size_t i = 0; i < dirHist->GetCount(); ++i)
+         dirs.push_back(dirHist->GetHistoryFile(i));
+   }
 
-	// if possible, have the validator of the combo box transfer the most recent directory to it when
-	if (!dirs.empty()) *dir = dirs[0]; // wxWindow::InitDialog() is called (i.e. as the dialog is being shown)
-	///
-	//// </_configuration_information_handling_> ////
+   // If possible, have the validator of the combo box transfer the most recent directory
+   // to it when wxWindow::InitDialog() is called (i.e. as the dialog is being shown).
+   if (!dirs.empty()) *dir = dirs[0];
+   ///
+   //// </_configuration_information_handling_> ////
 
-	wxBoxSizer* topSizer = new wxBoxSizer{wxVERTICAL};
+   wxBoxSizer* topSizer = new wxBoxSizer{wxVERTICAL};
 
-	wxBoxSizer* hBox = new wxBoxSizer{wxHORIZONTAL};
-	hBox->Add(new wxStaticText{this, wxID_ANY, "Your directory", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER},
-		1, wxALIGN_CENTER_VERTICAL);
-	hBox->Add(new wxButton{this, wxID_ANY, "Select Directory"});
+   wxBoxSizer* hBox = new wxBoxSizer{wxHORIZONTAL};
+   hBox->Add(new wxStaticText{this, wxID_ANY, "Your directory", wxDefaultPosition,
+      wxDefaultSize, wxALIGN_CENTER},
+      1, wxALIGN_CENTER_VERTICAL);
+   hBox->Add(new wxButton{this, wxID_ANY, "Select Directory"});
 
-	topSizer->Add(new wxStaticText{this, wxID_ANY,
-		"This wizard is meant to help you load a movie.\n\n"
-		"First, please select the directory containing your movie."});
-	topSizer->Add(0, 0, 1); // spacer with proportion of 1 ...
-	topSizer->Add(hBox, 0, wxEXPAND);
-	topSizer->Add(comboBox = new wxComboBox{this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, dirs,
-		0, DirValidator{dir}}, 0, wxEXPAND);
-	topSizer->Add(0, 0, 1); // ... and another identical spacer
-	topSizer->Add(new wxStaticText{this, wxID_ANY, "Click \"Next >\" once you are done!"});
+   topSizer->Add(new wxStaticText{this, wxID_ANY,
+      "This wizard is meant to help you load a movie.\n\n"
+      "First, please select the directory containing your movie."});
+   topSizer->Add(0, 0, 1); // spacer with proportion of 1 ...
+   topSizer->Add(hBox, 0, wxEXPAND);
+   topSizer->Add(comboBox = new wxComboBox{this, wxID_ANY, wxEmptyString,
+      wxDefaultPosition, wxDefaultSize, dirs, 0, DirValidator{dir}}, 0, wxEXPAND);
+   topSizer->Add(0, 0, 1); // ... and another identical spacer
+   topSizer->Add(new wxStaticText{this, wxID_ANY, "Click \"Next >\" once you are done!"});
 
-	comboBox->SetFocus();
+   comboBox->SetFocus();
 
-	topSizer->SetSizeHints(this);
-	SetSizer(topSizer);
+   topSizer->SetSizeHints(this);
+   SetSizer(topSizer);
 
-	Bind(wxEVT_COMMAND_BUTTON_CLICKED, &OpenMovieWizard::DirDialogPage::onButton, this, wxID_ANY);
+   Bind(wxEVT_COMMAND_BUTTON_CLICKED, &OpenMovieWizard::DirDialogPage::onButton, this,
+      wxID_ANY);
 }
 
 void OpenMovieWizard::DirDialogPage::onButton(wxCommandEvent&)
 {
-	const auto& dir = wxDirSelector(wxEmptyString, comboBox->GetValue(), // read the combo box for the default path
-		wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST, // the second style removes the button to create a new directory
-		wxDefaultPosition, this); // pass this as the parent
+   const auto& dir = wxDirSelector(wxEmptyString, comboBox->GetValue(), // read the combo box for the default path
+      wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST, // the second style removes the button to create a new directory
+      wxDefaultPosition, this); // pass this as the parent
 
-	if (!dir.empty()) // did the user select a directory?
-	{
-		if (comboBox->FindString(dir) == wxNOT_FOUND) {
-			comboBox->Append(dir);
-		}
-		comboBox->SetValue(dir);
-	}
+   if (!dir.empty()) // did the user select a directory?
+   {
+      if (comboBox->FindString(dir) == wxNOT_FOUND) {
+         comboBox->Append(dir);
+      }
+      comboBox->SetValue(dir);
+   }
 }
 
-OpenMovieWizard::RegExCtrlPage::RegExCtrlPage(OpenMovieWizard* parent, wxString* regEx, wxConfigBase* config)
-	: wxWizardPageSimple{parent}
+OpenMovieWizard::RegExCtrlPage::RegExCtrlPage(OpenMovieWizard* parent, wxString* regEx,
+   wxConfigBase* config) : wxWizardPageSimple{parent}
 {
-	wxControl* regExText;
+   wxControl* regExText;
 
-	//// <_configuration_information_handling_> ////
-	///
-	wxArrayString regExes{};
-	std::string   helpURL{};
+   //// <_configuration_information_handling_> ////
+   ///
+   wxArrayString regExes{};
+   std::string   helpURL{};
 
-	if (config)
-	{
-		const auto oldPath = config->GetPath();
+   if (config)
+   {
+      const auto oldPath = config->GetPath();
 
-		config->SetPath(u8"RegularExpressions"); // relative path
-		{
-			wxString key;
-			long     cookie; // this confusing variable is passed to the wxConfigBase enumeration functions
-			                 // by non-const reference; presumably in an attempted bribery
+      config->SetPath(u8"RegularExpressions"); // relative path
+      {
+         wxString key;
+         long     cookie; // This confusing variable is passed to the wxConfigBase
+                          // enumeration functions by non-const reference; presumably in
+                          // an attempt of bribery.
 
-			if (config->GetFirstEntry(key, cookie)) // note that apparently config->GetFirstEntry() and ...
-			{
-				regExes.push_back(config->Read(key));
-				while (config->GetNextEntry(key, cookie)) // ... config->GetNextEntry() retrieve keys
-				{                                         // in lexicographical order
-					regExes.push_back(config->Read(key));
-				}
-			}
-		} config->SetPath(oldPath);
+         if (config->GetFirstEntry(key, cookie))      // Note that config->GetFirstEntry()
+         {                                            // and...
+            regExes.push_back(config->Read(key));
+            while (config->GetNextEntry(key, cookie)) // ...config->GetNextEntry()
+            {                                         // seem to retrieve keys in
+               regExes.push_back(config->Read(key));  // lexicographical order
+            }
+         }
+      } config->SetPath(oldPath);
 
-		helpURL = config->Read(u8"RegExHelpURL", u8"");
-	}
+      helpURL = config->Read(u8"RegExHelpURL", u8"");
+   }
 
-	// if a URL could be obtained, a hyperlink control is used; otherwise just text
-	if (!helpURL.empty()) {
-		regExText = new wxHyperlinkCtrl{this, wxID_ANY, u8"(Perl-derived) Regular Expression", helpURL};
-	} else {
-		regExText = new wxStaticText{this, wxID_ANY, "(Perl-derived) Regular Expression"};
-	}
+   // if a URL could be obtained, a hyperlink control is used; otherwise just text
+   if (!helpURL.empty()) {
+      regExText = new wxHyperlinkCtrl{this, wxID_ANY,
+         u8"(Perl-derived) Regular Expression", helpURL};
+   } else {
+      regExText = new wxStaticText{this, wxID_ANY, "(Perl-derived) Regular Expression"};
+   }
 
-	if (!regExes.empty()) *regEx = regExes[0];
-	///
-	//// </_configuration_information_handling_> ////
+   if (!regExes.empty()) *regEx = regExes[0];
+   ///
+   //// </_configuration_information_handling_> ////
 
-	wxBoxSizer* topSizer = new wxBoxSizer{wxVERTICAL};
+   wxBoxSizer* topSizer = new wxBoxSizer{wxVERTICAL};
 
-	wxBoxSizer* hBox = new wxBoxSizer{wxHORIZONTAL};
-	hBox->Add(new wxStaticText{this, wxID_ANY, "Now you can provide a "});
-	hBox->Add(regExText);
+   wxBoxSizer* hBox = new wxBoxSizer{wxHORIZONTAL};
+   hBox->Add(new wxStaticText{this, wxID_ANY, "Now you can provide a "});
+   hBox->Add(regExText);
 
-	topSizer->Add(hBox);
-	topSizer->Add(new wxStaticText{this, wxID_ANY,
-		"that all images relevant to your movie need to match.\n\n"
-		"A few options can be selected from the drop-down list or,\n"
-		"to load all files in the directory, the text box may just be\n"
-		"left empty."});
-	topSizer->Add(0, 0, 1);
-	topSizer->Add(new wxStaticText{this, wxID_ANY, "Your regular expression"}, 0, wxALIGN_CENTER);
-	topSizer->Add(comboBox = new wxComboBox{this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, regExes,
-		0, wxTextValidator{wxFILTER_NONE, regEx}}, 0, wxEXPAND);
-	topSizer->Add(0, 0, 1);
-	topSizer->Add(new wxStaticText{this, wxID_ANY, "Click \"Finish\" once you are done!"});
+   topSizer->Add(hBox);
+   topSizer->Add(new wxStaticText{this, wxID_ANY,
+      "that all images relevant to your movie need to match.\n\n"
+      "A few options can be selected from the drop-down list or,\n"
+      "to load all files in the directory, the text box may just be\n"
+      "left empty."});
+   topSizer->Add(0, 0, 1);
+   topSizer->Add(new wxStaticText{this, wxID_ANY, "Your regular expression"}, 0,
+      wxALIGN_CENTER);
+   topSizer->Add(comboBox = new wxComboBox{this, wxID_ANY, wxEmptyString,
+      wxDefaultPosition, wxDefaultSize, regExes, 0,
+      wxTextValidator{wxFILTER_NONE, regEx}}, 0, wxEXPAND);
+   topSizer->Add(0, 0, 1);
+   topSizer->Add(new wxStaticText{this, wxID_ANY, "Click \"Finish\" once you are done!"});
 
-	comboBox->SetFocus();
+   comboBox->SetFocus();
 
-	topSizer->SetSizeHints(this);
-	this->SetSizer(topSizer);
+   topSizer->SetSizeHints(this);
+   this->SetSizer(topSizer);
 }
 
 //// <_DirValidator_member_definitions_> ////
 ///
 bool OpenMovieWizard::DirDialogPage::DirValidator::Validate(wxWindow* parent)
 {
-	wxTextEntry* window = dynamic_cast<wxTextEntry*>(GetWindow());
+   wxTextEntry* window = dynamic_cast<wxTextEntry*>(GetWindow());
 
-	assert(window != nullptr);
+   assert(window != nullptr);
 
-	wxString    msg;
-	const auto& data = window->GetValue();
-	bool        isValid = data != wxEmptyString;
+   wxString    msg;
+   const auto& data = window->GetValue();
+   bool        isValid = data != wxEmptyString;
 
-	if (!isValid) {
-		msg = u8"Required information entry is empty.";
-	} else if (!wxFileName::DirExists(data)) {
-		msg = u8"'" + window->GetValue() + u8"' should be an existing directory.";
-		isValid = false;
-	} else if (!wxFileName::IsDirReadable(data) || !wxFileName::IsDirWritable(data)) {
-		msg = u8"'" + window->GetValue() +
-				u8"' should be a directory that this process has read and write permissions on.";
-		isValid = false;
-	}
+   if (!isValid) {
+      msg = u8"Required information entry is empty.";
+   } else if (!wxFileName::DirExists(data)) {
+      msg = u8"'" + window->GetValue() + u8"' should be an existing directory.";
+      isValid = false;
+   } else if (!wxFileName::IsDirReadable(data) || !wxFileName::IsDirWritable(data)) {
+      msg = u8"'" + window->GetValue() + u8"' should be a directory that this process has"
+         "read and write permissions on.";
+      isValid = false;
+   }
 
-	if (!isValid) ::wxMessageBox(msg, u8"Validation conflict", wxOK | wxICON_EXCLAMATION, parent);
+   if (!isValid) {
+      ::wxMessageBox(msg, u8"Validation conflict", wxOK | wxICON_EXCLAMATION, parent);
+   }
 
-	return isValid;
+   return isValid;
 }
 
 bool OpenMovieWizard::DirDialogPage::DirValidator::TransferFromWindow()
 {
-	wxTextEntry* window = dynamic_cast<wxTextEntry*>(GetWindow());
-	assert(window != nullptr);
+   wxTextEntry* window = dynamic_cast<wxTextEntry*>(GetWindow());
+   assert(window != nullptr);
 
-	*value = window->GetValue();
-	return true;
+   *value = window->GetValue();
+   return true;
 }
 
 bool OpenMovieWizard::DirDialogPage::DirValidator::TransferToWindow()
 {
-	wxTextEntry* window = dynamic_cast<wxTextEntry*>(GetWindow());
-	assert(window != nullptr);
+   wxTextEntry* window = dynamic_cast<wxTextEntry*>(GetWindow());
+   assert(window != nullptr);
 
-	window->SetValue(*value);
-	return true;
+   window->SetValue(*value);
+   return true;
 }
 
 wxObject* OpenMovieWizard::DirDialogPage::DirValidator::Clone() const
 {
-	return new DirValidator{*this};
+   return new DirValidator{*this};
 }
 ///
 //// </_DirValidator_member_definitions_> ////
+
+// vim: tw=90 sw=3 et
--- open_movie_wizard.h
+++ open_movie_wizard.h
@@ -7,21 +7,24 @@
 
 class OpenMovieWizard : public wxWizard
 {
-	public:
+   public:
 
-	OpenMovieWizard(wxWindow* parent, int id = wxID_ANY, wxString* dir = nullptr, wxString* regEx = nullptr,
-		wxConfigBase* = nullptr, const wxFileHistory* dirHistory = nullptr);
+   OpenMovieWizard(wxWindow* parent, int id = wxID_ANY, wxString* dir = nullptr,
+      wxString* regEx = nullptr, wxConfigBase* = nullptr,
+      const wxFileHistory* dirHistory = nullptr);
 
-	// execute the wizard starting at dirPage; return true if it was successfully finished,
-	bool RunWizard(); // false if the user cancelled it
+   // Execute the wizard starting at dirPage; return true if it was successfully finished,
+   bool RunWizard(); // false if the user cancelled it.
 
-	private:
+   private:
 
-	class DirDialogPage;
-	class RegExCtrlPage;
+   class DirDialogPage;
+   class RegExCtrlPage;
 
-	DirDialogPage* dirPage;
-	RegExCtrlPage* regExPage;
+   DirDialogPage* dirPage;
+   RegExCtrlPage* regExPage;
 };
 
 #endif //OPEN_MOVIE_WIZARD_H
+
+// vim: tw=90 sw=3 et
