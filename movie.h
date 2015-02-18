bb679cfc94767301f672550c4baf5c6ec3b35359
Lukas Waymann <lb.waym@gmail.com>
2015-02-18 12:38:09 +0100

    Update formatting, add modelines

--- movie.h
+++ movie.h
@@ -7,66 +7,68 @@
 #include <string>
 #include <vector>
 
-#include <boost/regex.hpp> // note: switch to <regex> from the stdlib when upgrading to a future release of GCC
+#include <boost/regex.hpp> // Note: switch to <regex> from the stdlib when upgrading to a
+                           // future release of GCC.
 
-//#define BOOST_THREAD_VERSION=3       // ...
 #define BOOST_THREAD_USE_LIB
-#include <boost/thread.hpp>     // thread
+#include <boost/thread.hpp> // thread
 
 #include "frame.h"
 
 class Movie
 {
-	public:
+   public:
 
-	Movie() = default;
-	Movie(const Movie&) = delete;
-	Movie(Movie&&);
-	Movie(const std::string& directory, const std::string& regEx);
+   Movie() = default;
+   Movie(const Movie&) = delete;
+   Movie(Movie&&);
+   Movie(const std::string& directory, const std::string& regEx);
 
-	~Movie();
+   ~Movie();
 
-	Movie& operator=(const Movie&) = delete;
-	Movie& operator=(Movie&&);
+   Movie& operator=(const Movie&) = delete;
+   Movie& operator=(Movie&&);
 
-	const std::string& getDir() const;
-	const Frame& getFrame(std::size_t) const;
-	std::string getFilename(std::size_t) const; // includes directory
-	std::size_t getSize() const;
-	std::size_t size() const;
+   const std::string& getDir() const;
+   const Frame& getFrame(std::size_t) const;
+   std::string getFilename(std::size_t) const; // includes directory
+   std::size_t getSize() const;
+   std::size_t size() const;
 
-	private:
+   private:
 
-	void populateBuffer();
+   void populateBuffer();
 
-	std::string* dir;
-	std::vector<Frame> frames;
+   std::string* dir;
+   std::vector<Frame> frames;
 
-	std::deque<std::shared_ptr<const Bitmap>> bitmapBuffer;
+   std::deque<std::shared_ptr<const Bitmap>> bitmapBuffer;
 
-	bool terminateThread;
-	boost::mutex terminateFlagAccess;
-	boost::thread thread;
+   bool terminateThread;
+   boost::mutex terminateFlagAccess;
+   boost::thread thread;
 };
 
 inline const std::string& Movie::getDir() const {
-	return *dir;
+   return *dir;
 }
 
 inline const Frame& Movie::getFrame(std::size_t i) const {
-	return frames[i];
+   return frames[i];
 }
 
 inline std::string Movie::getFilename(std::size_t i) const {
-	return *dir + frames[i].getFilename();
+   return *dir + frames[i].getFilename();
 }
 
 inline std::size_t Movie::getSize() const {
-	return frames.size();
+   return frames.size();
 }
 
 inline std::size_t Movie::size() const {
-	return frames.size();
+   return frames.size();
 }
 
 #endif //MOVIE_H
+
+// vim: tw=90 sw=3 et
