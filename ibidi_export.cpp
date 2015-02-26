03f8bf113222fcfbd66a52868eb283e18f651443
Lukas Waymann <meribold@gmail.com>
2015-02-26 14:14:00 +0100

    Sort trackees differently for ibidi export
    Trackees are sorted using integer comparison of their identifiers now
    (ascending order, operator<). Trackees whose identifiers are not
    integers are skipped (no change).

--- ibidi_export.cpp
+++ ibidi_export.cpp
@@ -1,4 +1,6 @@
-#include <fstream> // ofstream
+#include <algorithm> // sort
+#include <fstream>   // ofstream
+#include <vector>
 
 #include <wx/msgdlg.h>
 
@@ -7,51 +9,59 @@
 void ibidiExport(const std::string& fileName,
    const std::map<std::string, Trackee>& trackees)
 {
-   std::ofstream oStream{fileName}; // RAII
+   std::vector<int> goodTrackees{};
+   std::string badTrackees{};
 
-   oStream << "--\ttrack\tslice\tx\ty\n";
-
-   std::string skippedTrackees{};
-
-   unsigned i = 1;
    for (const auto& pair : trackees)
    {
       const std::string trackeeID = std::get<0>(pair);
 
       int index;
+
       try {
          index = std::stoi(trackeeID);
       }
       catch (std::invalid_argument) {
-         skippedTrackees += trackeeID + ", ";
+         badTrackees += trackeeID + ", ";
          continue;
       }
       catch (std::out_of_range) {
-         skippedTrackees += trackeeID + ", ";
+         badTrackees += trackeeID + ", ";
          continue;
       }
 
       if (index <= 0 || std::to_string(index) != trackeeID) {
-         skippedTrackees += trackeeID + ", ";
+         badTrackees += trackeeID + ", ";
       }
       else {
-         const Trackee trackee = std::get<1>(pair);
-         // trackee.getTrack() returns a weak_ptr. weak_ptr::lock() returns a shared_ptr.
-         const Track track = *trackee.getTrack().lock();
-
-         unsigned j = 0;
-         for (; j < track.size(); ++j)
-         {
-            oStream << i + j << '\t' << trackeeID << '\t' << j + 1 << '\t' << track[j].x
-                    << '\t' << track[j].y << '\n';
-         }
-         i += j;
+         goodTrackees.push_back(index);
       }
    }
 
-   if (!skippedTrackees.empty()) {
+   std::sort(goodTrackees.begin(), goodTrackees.end());
+
+   std::ofstream oStream{fileName}; // RAII
+   oStream << "--\ttrack\tslice\tx\ty\n";
+
+   unsigned i = 1;
+   for (int index : goodTrackees)
+   {
+      const Trackee& trackee = trackees.at(std::to_string(index));
+      // trackee.getTrack() returns a weak_ptr. weak_ptr::lock() returns a shared_ptr.
+      const Track track = *trackee.getTrack().lock();
+
+      unsigned j = 0;
+      for (; j < track.size(); ++j)
+      {
+         oStream << i + j << '\t' << index << '\t' << j + 1 << '\t' << track[j].x << '\t'
+                 << track[j].y << '\n';
+      }
+      i += j;
+   }
+
+   if (!badTrackees.empty()) {
       ::wxMessageBox("Some trackees were skipped because of incompatible identifiers: " +
-         skippedTrackees.erase(skippedTrackees.size() - 2), "Warning", wxOK | wxCENTRE,
+         badTrackees.erase(badTrackees.size() - 2), "Warning", wxOK | wxCENTRE,
          nullptr);
    }
 }
