a8147e8ad5f14c87c561f24e3857501cff3ccc91
Lukas Waymann <lb.waym@gmail.com>
2015-10-12 16:35:50 +0200

    Use only 1 bit per pixel for splash bitmaps

--- main_frame.cpp
+++ main_frame.cpp
@@ -6,9 +6,13 @@
 #include <fstream>    // ofstream
 #include <functional> // bind
 #include <iomanip>    // stream manipulator functions setfill() and setw()
-#include <memory>     // shared_ptr
+#include <memory>     // shared_ptr, unique_ptr
 #include <sstream>    // stringstream
+#include <cstdint>
+#include <climits>
 #include <string>
+
+#include <boost/detail/endian.hpp>
 
 #include <wx/aboutdlg.h>    // wxAboutBox()
 #include <wx/filehistory.h> // wxFileHistory
@@ -784,7 +788,14 @@ template <std::size_t N>
 void createBitmaps(std::string name)
 {
    constexpr unsigned SCALE_FACTOR = 24;
-   std::array<std::array<char, 24 * 24 / 8>, N> xBitMaps;
+
+   // After changing this function to allocate a lot more memory for objects with
+   // automatic storage duration, some of that memory started to look kind of nonrandom.
+   // Dynamic storage duration seems to result in subjectively much more random memory.
+   std::unique_ptr<std::array<std::array<char, 24 * 24 / 8>, N>>
+      xBitMapsPointer{new std::array<std::array<char, 24 * 24 / 8>, N>};
+
+   std::array<std::array<char, 24 * 24 / 8>, N>& xBitMaps = *xBitMapsPointer;
 
    for (std::size_t bitmapNum = 0; bitmapNum < N; ++bitmapNum)
    {
@@ -805,23 +816,100 @@ void createBitmaps(std::string name)
       std::stringstream sStream;
       sStream << std::setfill('0') << std::setw(2) << bitmapNum;
 
-      // The image often is blurred when being scaled up later (on my Windows system it
-      // wasn't, iirc), so we don't save it like this. Using really small bitmaps also
-      // caused some visual artifacts later.
+      // I was going to use std::int8_t in case there are more than 8 bits in a char, but
+      // how can there be an int8_t then?
+      #if (CHAR_BIT != 8)
+        #error
+      #endif
+
+      #ifndef BOOST_LITTLE_ENDIAN
+        #error
+      #endif
+
+      std::ofstream bitmapStream(name + sStream.str() + ".bmp", std::ios::binary);
+
+      constexpr std::uint32_t dibHeaderSize = 40;
+
+      char pixelArray[24 * SCALE_FACTOR * 24 * SCALE_FACTOR / 8];
+
+      constexpr char colorTable[8] = {
+         0x00, 0x00, 0x00, 0x00,
+         0x20, 0x20, 0x20, 0x00
+      };
+
+      constexpr std::uint32_t fileSize = 14 + dibHeaderSize + sizeof(colorTable) +
+         sizeof(pixelArray);
+      constexpr std::uint32_t offset   = 14 + dibHeaderSize + sizeof(colorTable);
+
+      const char bitmapFileHeader[14] = {
+         0x42, 0x4d, // "BM"
+         reinterpret_cast<const char*>(&fileSize)[0],
+         reinterpret_cast<const char*>(&fileSize)[1],
+         reinterpret_cast<const char*>(&fileSize)[2],
+         reinterpret_cast<const char*>(&fileSize)[3],
+         0, 0, 0, 0,
+         reinterpret_cast<const char*>(&offset)[0],
+         reinterpret_cast<const char*>(&offset)[1],
+         reinterpret_cast<const char*>(&offset)[2],
+         reinterpret_cast<const char*>(&offset)[3],
+      };
+
+      // This will be supported soonish!  Remember to change the color table to 3 byte per
+      // entry.  See github.com/wxWidgets/wxWidgets/commit/371928415ab3cf79647153f96907838
       /*
-      wxBitmap bitmap{bits.data(), 24, 24};
-      bitmap.SaveFile(name + sStream.str() + ".bmp", wxBITMAP_TYPE_BMP);
+      std::uint16_t bitmapWidth   = 24 * SCALE_FACTOR;
+      std::uint16_t bitmapHeight  = 24 * SCALE_FACTOR;
+      std::uint16_t one           = 1;
+
+      const char bitmapCoreHeader[dibHeaderSize] = {
+         reinterpret_cast<const char*>(&dibHeaderSize)[0],
+         reinterpret_cast<const char*>(&dibHeaderSize)[1],
+         reinterpret_cast<const char*>(&dibHeaderSize)[2],
+         reinterpret_cast<const char*>(&dibHeaderSize)[3],
+         reinterpret_cast<const char*>(&bitmapWidth)[0],
+         reinterpret_cast<const char*>(&bitmapWidth)[1],
+         reinterpret_cast<const char*>(&bitmapHeight)[0],
+         reinterpret_cast<const char*>(&bitmapHeight)[1],
+         reinterpret_cast<const char*>(&one)[0], // Number of color planes, must be 1
+         reinterpret_cast<const char*>(&one)[1],
+         reinterpret_cast<const char*>(&one)[0], // Number of bits per pixel
+         reinterpret_cast<const char*>(&one)[1],
+      };
       */
 
-      // Using wxWidgets' scaling functions directly also blurs the image (dependent on
-      // platform; on my Windows system it didn't, iirc): not doing this either.
-      /*
-      wxImage image = bitmap.ConvertToImage();
-      image.Rescale(384, 384, wxIMAGE_QUALITY_NEAREST);
-      */
+      std::int32_t  bitmapWidth  = 24 * SCALE_FACTOR;
+      std::int32_t  bitmapHeight = 24 * SCALE_FACTOR;
+      std::uint16_t one          = 1;
+      std::uint32_t imageSize    = sizeof(pixelArray);
 
-      wxImage image{24 * SCALE_FACTOR, 24 * SCALE_FACTOR};
-      unsigned char* data = image.GetData();
+      const char bitmapInfoHeader[dibHeaderSize] = {
+         reinterpret_cast<const char*>(&dibHeaderSize)[0],
+         reinterpret_cast<const char*>(&dibHeaderSize)[1],
+         reinterpret_cast<const char*>(&dibHeaderSize)[2],
+         reinterpret_cast<const char*>(&dibHeaderSize)[3],
+         reinterpret_cast<const char*>(&bitmapWidth)[0],
+         reinterpret_cast<const char*>(&bitmapWidth)[1],
+         reinterpret_cast<const char*>(&bitmapWidth)[2],
+         reinterpret_cast<const char*>(&bitmapWidth)[3],
+         reinterpret_cast<const char*>(&bitmapHeight)[0],
+         reinterpret_cast<const char*>(&bitmapHeight)[1],
+         reinterpret_cast<const char*>(&bitmapHeight)[2],
+         reinterpret_cast<const char*>(&bitmapHeight)[3],
+         reinterpret_cast<const char*>(&one)[0], // Number of color planes, must be 1
+         reinterpret_cast<const char*>(&one)[1],
+         reinterpret_cast<const char*>(&one)[0], // Number of bits per pixel
+         reinterpret_cast<const char*>(&one)[1],
+         0, 0, 0, 0,
+         reinterpret_cast<const char*>(&imageSize)[0],
+         reinterpret_cast<const char*>(&imageSize)[1],
+         reinterpret_cast<const char*>(&imageSize)[2],
+         reinterpret_cast<const char*>(&imageSize)[3],
+         0, 0, 0, 0,
+         0, 0, 0, 0,
+         2, 0, 0, 0,
+         2, 0, 0, 0
+      };
+
       for (unsigned rowNum = 0; rowNum < 24; ++rowNum)
       {
          for (unsigned colNum = 0; colNum < 24; ++colNum)
@@ -831,38 +919,20 @@ void createBitmaps(std::string name)
             bool set = (bits[byteNum] >> bitNum) & 1;
             for (unsigned i = 0; i < SCALE_FACTOR; ++i)
             {
-               for (unsigned j = 0; j < SCALE_FACTOR; ++j)
+               for (unsigned j = 0; j < SCALE_FACTOR / 8; ++j)
                {
-                  int pixelNum = SCALE_FACTOR * SCALE_FACTOR * 24 * rowNum +
-                     SCALE_FACTOR * colNum + 24 * SCALE_FACTOR * i + j;
-                  data[3 * pixelNum] = set ? 0x20 : 0x00;
-                  // Don't need green and blue channels.
+                  int pixelNum = (SCALE_FACTOR * SCALE_FACTOR * 24 * (23 - rowNum) +
+                     SCALE_FACTOR * colNum + 24 * SCALE_FACTOR * i) / 8 + j;
+                  pixelArray[pixelNum] = set ? 0xff : 0x00;
                }
             }
          }
       }
 
-      // I don't know how to save using 1 bit per pixel (bpp) and a custom palette...
-
-      // ... this doesn't work...
-      /*
-      constexpr unsigned char colorValues[2] = {0x20, 0x00};
-      const wxPalette palette{2, colorValues, colorValues, colorValues};
-      image.SetPalette(palette);
-      image.SetOption(wxIMAGE_OPTION_BMP_FORMAT, wxBMP_8BPP_PALETTE);
-      image.SaveFile(name + sStream.str() + ".bmp", wxBITMAP_TYPE_BMP);
-      */
-
-      // ... neither does this...
-      /*
-      wxBitmap bitmap{image, 1};
-      bitmap.SetPalette(palette);
-      bitmap.SaveFile(name + sStream.str() + ".bmp", wxBITMAP_TYPE_BMP, &palette);
-      */
-
-      // ... so I'm using 8-bit grayscale. See "wx/image.h".
-      image.SetOption(wxIMAGE_OPTION_BMP_FORMAT, wxBMP_8BPP_RED);
-      image.SaveFile(name + sStream.str() + ".bmp", wxBITMAP_TYPE_BMP);
+      bitmapStream.write(bitmapFileHeader, sizeof(bitmapFileHeader));
+      bitmapStream.write(bitmapInfoHeader, dibHeaderSize);
+      bitmapStream.write(colorTable, sizeof(colorTable));
+      bitmapStream.write(pixelArray, sizeof(pixelArray));
    }
 }
 ///
